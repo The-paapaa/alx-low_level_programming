6107134f8325b4160a53c445d7923ff06ea510aa
Prevailer <edwardprevail@gmail.com>
2023-05-24 19:15:10 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * main -main function
+  * _putchar - prints alphabet
+  * Return: always (0)
+  */
+
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
