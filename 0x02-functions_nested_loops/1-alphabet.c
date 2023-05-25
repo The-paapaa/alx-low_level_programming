07398f7bc722503ca01e1715fb9b9d22a93dff5b
Prevailer <edwardprevail@gmail.com>
2023-05-24 19:35:29 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * void print_alphabet(void) - prints alphabet in lower case
+  *
+  *Return: always 0;
+  */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = "a" ; 1 <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+
+	return = (0)
+}
